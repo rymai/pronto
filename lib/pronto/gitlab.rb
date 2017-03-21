c70db535d085aeceb425363cdaecd436a16c23be
Rémy Coutable <remy@rymai.me>
2017-03-21 23:56:04 +0100

    Add stub support for GitLab MR comments
    GitLab API doesn't currently support posting a comment on a merge
    request diff, so we fallback to commenting on the commit instead.
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- lib/pronto/gitlab.rb
+++ lib/pronto/gitlab.rb
@@ -1,5 +1,13 @@
 module Pronto
   class Gitlab < Client
+    def pull_comments(sha)
+      @comment_cache["#{mr_id}/#{sha}"] ||= begin
+        client.merge_request_comments(slug, mr_id).map do |comment|
+          Comment.new(sha, comment.note, 42, 42)
+        end
+      end
+    end
+
     def commit_comments(sha)
       @comment_cache[sha.to_s] ||= begin
         client.commit_comments(slug, sha, per_page: 500).map do |comment|
@@ -13,6 +21,11 @@ module Pronto
       client.create_commit_comment(slug, comment.sha, comment.body,
                                    path: comment.path, line: comment.position,
                                    line_type: 'new')
+    end
+
+    def create_pull_comment(comment)
+      @config.logger.log("GitLab does not have an API to create MR diff comments yet.")
+      create_commit_comment(comment)
     end
 
     private
@@ -43,6 +56,26 @@ module Pronto
     def client
       @client ||= ::Gitlab.client(endpoint: gitlab_api_endpoint,
                                   private_token: gitlab_api_private_token)
+    end
+
+    def mr_id
+      mr ? mr[:number].to_i : env_pull_id.to_i
+    end
+
+    def mr_sha
+      mr[:sha] if mr
+    end
+
+    def mr
+      @pull ||= if env_pull_id
+                  merge_requests.find { |mr| mr[:iid].to_i == env_pull_id.to_i }
+                elsif @repo.branch
+                  merge_requests.find { |mr| mr[:source_branch] == @repo.branch }
+                end
+    end
+
+    def merge_requests
+      @merge_requests ||= client.merge_requests(slug)
     end
 
     def gitlab_api_private_token
